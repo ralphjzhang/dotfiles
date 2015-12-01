e02aba9bf2437b12ca91ea70d0e27a4ab35f9197
zhjun <ralph.j.zhang@gmail.com>
2015-12-01 17:28:57 +0800

    changed ycm conf

--- vim/.ycm_extra_conf.py
+++ vim/.ycm_extra_conf.py
@@ -38,9 +38,6 @@ flags = [
 '-Wall',
 '-Wextra',
 '-Werror',
-'-Wc++98-compat',
-'-Wno-long-long',
-'-Wno-variadic-macros',
 '-fexceptions',
 '-DNDEBUG',
 # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
@@ -52,7 +49,7 @@ flags = [
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-'-std=c++11',
+'-std=c++14',
 '-stdlib=libc++',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
